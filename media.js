8dc97c4085e1dfdbf35485cb2315d430dea64675
douglasdarla <112879752+douglasdarla@users.noreply.github.com>
2023-09-18 12:35:53 -0300

    18/09 - 2 aula

---
+++ media.js
@@ -0,0 +1,5 @@
+//Cria referência ao form e elementos de resposta do programa
+
+const form = document.querySelector("form")
+const resp1 = document.querySelector("h3")
+const resp2 = document.querySelector("h4")
